28079be01e608ea377619ca8f9339c57d14bf0bd
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-05-23 17:34:26 +0800

    Add finished centralised todo-list

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+const tasks = ["buy milk", "eat dinner", "nail javascript", "give feedback"];
+
+const createElement = value => {
+  let li = document.createElement("li");
+  li.textContent = value;
+  toDoList.appendChild(li);
+};
+
+const toDoList = document.getElementById("todo-list");
+for (let task of tasks) {
+  createElement(task);
+}
+
+toDoList.addEventListener("click", event => {
+  event.target.classList.toggle("done");
+});
+
+// create element and listen to keyup event for input
+const input = document.querySelector("input");
+input.addEventListener("keyup", event => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    createElement(input.value);
+  }
+});
+
+// create element and listen to click event for button
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  createElement(input.value);
+});
